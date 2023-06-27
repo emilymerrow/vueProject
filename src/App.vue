9f57625b736a64665f72f668f2ff361ae19f056c
emilymerrow <emilyanddavidmerrow@gmail.com>
2023-06-26 20:38:59 -0400

    First commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <section>
+    <h2>My Friends</h2>
+    <ul>
+       <friend-contact></friend-contact>
+       <friend-contact></friend-contact>
+       <friend-contact></friend-contact>
+    </ul>
+    </section>
+</template>
+
+<script>
+import FriendContact from './components/FriendContact.vue'
+export default {
+  components: { FriendContact },
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '0123 5648393',
+                    email: 'manuel@localhost.com'
+                },
+                {
+                    id: 'julie',
+                    name: 'Julie Jones',
+                    phone: '0123 56489003',
+                    email: 'julie@localhost.com'
+                },
+            ]
+        }
+    }
+}
+
+</script>
